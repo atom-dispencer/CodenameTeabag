7cf636941295669d194cd9ed96e6badbdabd3690
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-05-20 20:26:19 +0100

    Lesson 0 testing script
    Also some small edits to the main README to make it sound nicer

---
+++ lesson-0/codename-teabag-0.py
@@ -0,0 +1,25 @@
+#
+# Codename Teabag Programming Tutorial
+# Copyright (C) 2024 Adam Spencer
+#
+# This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+print("Greetings, Adventurer!")
+
+print("What do they call you?")
+name = input("> ")
+
+print("Greetings to you, " + name + "!")
+print("You may now proceed to the Forgotten Lands...")
